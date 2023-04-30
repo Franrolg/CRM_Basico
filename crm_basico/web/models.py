5b78135cd94cfce875b2bee9680d60f02fa34d08
franrolg <francisco.roldang0@gmail.com>
2023-04-30 07:29:57 -0400

    feat: modelos cliente, direccion y ciudad

--- crm_basico/web/models.py
+++ crm_basico/web/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Direccion(models.Model):
+    calle = models.CharField(max_length=100)
+    numero = models.PositiveIntegerField()
+    sector = models.CharField(max_length=50, null=True)
+    ciudad = models.ForeignKey(Ciudad, models.CASCADE)
+
+    def __str__(self) -> str:
+        return(f'{self.calle} {self.numero}, {self.ciudad}')
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.OneToOneField(Direccion, models.CASCADE)
+    email = models.CharField(max_length=100, unique=True)
+    telefono = models.CharField(max_length=12)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return(f'{self.nombre} {self.apellido}')
