34deceda0f881d37edaf3ed9021af4280a9290e4
franrolg <francisco.roldang0@gmail.com>
2023-04-30 05:34:35 -0400

    fix: placeholder de los campos de contraseña en el form de registro

--- crm_basico/web/forms.py
+++ crm_basico/web/forms.py
@@ -22,11 +22,11 @@ class FormularioRegistroUsuario(UserCreationForm):
         self.fields['username'].help_text = '<span class="form-text text-muted"><small>Requerido. Solo puedes usar letras, números y los símbolos @/./+/-/_.</small></span>'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Contraseña'
         self.fields['password1'].label = ''
         self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Tu contraseña no puede ser similar a tu información personal.</li><li>Tu contraseña debe contener al menos 8 caracteres.</li><li>Tu contraseña no puede ser una usada comúnmente.</li><li>Tu contraseña no puede contener solo números.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Repetir contraseña'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Debes ingresar la misma contraseña que la anterior.</small></span>'
