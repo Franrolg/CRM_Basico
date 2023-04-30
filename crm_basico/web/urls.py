f497c7536c88b5246f3764e1a958026d4532c17c
franrolg <francisco.roldang0@gmail.com>
2023-04-30 04:58:40 -0400

    feat: se agregar inicio y cierre de sesión

--- crm_basico/web/urls.py
+++ crm_basico/web/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('cerrar-sesion/', views.cerrar_sesion, name='cerrar sesion'),
 ]
--- crm_basico/web/views.py
+++ crm_basico/web/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
+
+    if request.method == 'POST':
+        usuario = request.POST['usuario']
+        contrasena = request.POST['contrasena']
+
+        user = authenticate(request, username=usuario, password=contrasena)
+
+        if user: 
+            login(request, user)
+            messages.success(request, "¡Has iniciado sesión!")
+        else: 
+            messages.error(request, "¡Error al iniciar sesión!")
+        
+        return redirect('index')
+
     return render(request, 'index.html')
+
+def cerrar_sesion(request):
+    logout(request)
+    messages.success(request, "¡Has cerrado sesión!")
+    return redirect('index')
